81d39b69fe715cbb3b9a9b74657b9fc9f6f8fde7
Eyob-Se <messieyob346@gmail.com>
2025-05-14 17:09:22 +0300

    Initial psuh of Elu-project

---
+++ views/available_house.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style.css">
+    <title>Available house</title>
+</head>
+<body>
+    <div class="prop_con">
+        
+        <div class="navbar prop_nav">
+            <p>Rental.</p>
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="available_house.php">Available house</a></li>
+                <li><a href="about_us.php">About us</a></li>
+            </ul>
+            <button>
+                <a href="login_page.php">Login</a>
+            </button>
+        </div>
+        <div class="cards">
+            <div class="card c1">
+                <img src="../assets/prop_img/h1.jpg" alt="House 1">
+                <h3>House 1</h3>
+                <p>Location: City Center</p>
+                <p>Price: $1200/month</p>
+                <button><a href="#">View Details</a></button>
+            </div>
+            <div class="card c2">
+                <img src="../assets/prop_img/h2.jpg" alt="House 2">
+                <h3>House 2</h3>
+                <p>Location: Suburb</p>
+                <p>Price: $800/month</p>
+                <button><a href="#">View Details</a></button>
+            </div>
+            <div class="card c3">
+                <img src="../assets/prop_img/h3.jpg" alt="House 3">
+                <h3>House 3</h3>
+                <p>Location: Downtown</p>
+                <p>Price: $1500/month</p>
+                <button><a href="#">View Details</a></button>
+            </div>
+            <div class="card c4">
+                <img src="../assets/prop_img/h4.jpg" alt="House 3">
+                <h3>House 3</h3>
+                <p>Location: Downtown</p>
+                <p>Price: $1500/month</p>
+                <button><a href="#">View Details</a></button>
+            </div><div class="card c5">
+                <img src="../assets/prop_img/h5.jpg" alt="House 3">
+                <h3>House 3</h3>
+                <p>Location: Downtown</p>
+                <p>Price: $1500/month</p>
+                <button><a href="#">View Details</a></button>
+            </div>
+            <div class="card c6">
+                <img src="../assets/prop_img/h6.jpg" alt="House 3">
+                <h3>House 3</h3>
+                <p>Location: Downtown</p>
+                <p>Price: $1500/month</p>
+                <button><a href="#">View Details</a></button>
+            </div>
+     
+        </div>
+      </div>
+
+        <!-- Add your content here -->
+    </div>
+</body>
+</html>
